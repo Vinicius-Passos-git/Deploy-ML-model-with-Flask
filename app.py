f1717e681961d2e47d56787cd1fac584be03978e
Vinicius-Passos-git <66977748+Vinicius-Passos-git@users.noreply.github.com>
2021-10-06 20:36:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
 
 app = Flask(__name__)
 
@@ -16,24 +11,23 @@ def home():
 
 @app.route('/result', methods = ['POST'])
 def result():
-    if request.method == 'POST':
-        year = request.form.get('year', type = int)
-        manufacturer = request.form.get('manufacturer')
-        odometer = request.form.get('odometer')
-        transmission = request.form.get('transmission')
-        type = request.form.get('type')
+    # if request.method == 'POST':
+    year = request.form.get('year', type = int)
+    manufacturer = request.form.get('manufacturer')
+    odometer = request.form.get('odometer')
+    transmission = request.form.get('transmission')
+    type = request.form.get('type')
 
-        cols = ['year', 'manufacturer', 'odometer', 'transmission', 'type']
-        test_set = pd.DataFrame([[year, manufacturer, odometer, transmission, type]], columns=cols)
+    cols = ['year', 'manufacturer', 'odometer', 'transmission', 'type']
+    test_set = pd.DataFrame([[year, manufacturer, odometer, transmission, type]], columns=cols)
         
-        model = pickle.load(open("model.pkl", "rb"))
+    model = pickle.load(open("model.pkl", "rb"))
 
-        preds = model.predict(test_set)
-        predicted = str(preds[0])
-        return 'The Predicted Car Value is: {}'.format(predicted)
+    preds = model.predict(test_set)
+    predicted = round((preds[0]),2)
+    return  render_template("home.html", prediction_text = 'The value of the predicted car is: {}'.format(predicted))
 
 #Used to puu debug into code. If you make a change in code, you just need
 #update the website and the application of changes will work
 if __name__ == '__main__':
-    app.run(debug = True) 
-    
+    app.run(debug = True)
